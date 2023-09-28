75abb5e749ba75b0939be3a4dd84e19995a01993
MaximoAF <128649465+MaximoAF@users.noreply.github.com>
2023-09-28 12:15:48 -0300

    Add files via upload

---
+++ Trabajo_Practico_2_java/src/Ejercicio_2_Traba_Practico_String.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.sql.Array;
+
+public class Ejercicio_2_Traba_Practico_String {
+    public static void main(String[] args) {
+        /* 2)_Dado una secuencia de no nulos, calcular el mayor de los negativos
+        y el promedio de los positivos.*/
+
+
+        int [][] ArrayNumber =;
+        int Sumatory = 0;
+        int Average = 0;
+        int BiggerNumber = 0;
+
+        for (int i; i == ArrayNumber; i++){
+            Number = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero: (Ingrese 0 para salir) "));
+            if (Number > 0) {
+                Sumatory += Number;
+                Average += 1;
+            } else {
+                if (Number < BiggerNumber){
+                    BiggerNumber = Number;
+                }
+            }
+        }
+
+        JOptionPane.showInputDialog(null,
+                "El mayor de los n")
+
+    }
+}
