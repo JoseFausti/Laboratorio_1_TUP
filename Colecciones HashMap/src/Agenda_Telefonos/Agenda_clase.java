a2e2d52a74683b10cbcc0edc6fa9b3bc4a7b228d
JoseFausti <141734639+JoseFausti@users.noreply.github.com>
2023-11-09 14:51:50 -0300

    Add files via upload

--- Colecciones HashMap/src/Agenda_Telefonos/Agenda_clase.java
+++ Colecciones HashMap/src/Agenda_Telefonos/Agenda_clase.java
@@ -3,55 +3,49 @@ package Agenda_Telefonos;
 import java.util.HashMap;
 
 public class Agenda_clase {
-    public class Agenda{
-        // Definicion de Variables
-        private  String nombre;
-        private int numero;
-        private HashMap<String,Integer> Agenda;
-        // Constructor
-        public Agenda(String nombre, int numero) {
-            this.nombre = nombre;
-            this.numero = numero;
-            this.Agenda = new HashMap<String,Integer>();
-        }
-        // Setters
-        public void setNombre(String nombre) {
-            this.nombre = nombre;
-        }
+    // Definicion de Variables
+    private  String  nombre;
+    private int telefono;
+    private HashMap<String,Integer> Agenda;
 
-        public void setNumero(int numero) {
-            this.numero = numero;
-        }
+    public Agenda_clase(){}
 
-        public void setAgenda(HashMap<String, Integer> agenda) {
-            Agenda = agenda;
-        }
+    // Constructor
+    public Agenda_clase(String nombre, int numero) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.Agenda = new HashMap<String,Integer>();
+    }
+    // Setters
+    public void setNombre(String nombre) {this.nombre = nombre;}
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+    public void setAgenda(HashMap<String, Integer> agenda) {this.Agenda = agenda;}
 
-        // Getters
-        public String getNombre() {
-            return nombre;
-        }
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+    public int getTelefono() {return telefono;}
+    public HashMap<String, Integer> getAgenda() {
+        return Agenda;
+    }
 
-        public int getNumero() {
-            return numero;
-        }
-        public HashMap<String, Integer> getAgenda() {
-            return Agenda;
-        }
+    //Métodos
     public void AgregarContacto(String nombre, Integer telefono){
             if(Agenda.containsKey(nombre) == false){
                 Agenda.put(nombre,telefono);
             }
     }
     public Integer BuscarNombre(String nombreEnLaAgenda){
-            if (nombreEnLaAgenda != null){
+            if(nombreEnLaAgenda != null){
                 return Agenda.get(nombreEnLaAgenda);
             } // retorna el valor
         return 0;
     }
-
     public void MostrarAgenda(){
         System.out.println(Agenda.entrySet());;
-        }
     }
 }
+
---
+++ Colecciones HashMap/src/Main_Agenda.java
@@ -0,0 +1,66 @@
+import Agenda_Telefonos.*;
+import javax.swing.*;
+import java.util.HashMap;
+
+public class Main_Agenda {
+
+    public static void main(String[] args) {
+        String nombre;
+        int telefono;
+
+        Agenda_clase persona = new Agenda_clase();
+        HashMap<String, Integer> persona_agenda = new HashMap<String, Integer>();
+
+        String salir = "";
+        int option;
+
+        JOptionPane.showMessageDialog(null, "Cree la Agenda con los nombres de personas y numeros de telefono..");
+
+        while (true) {
+            nombre = JOptionPane.showInputDialog("Ingrese un nombre:");
+            telefono = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su nº de telefono: "));
+
+            persona.setNombre(nombre);
+            persona.setTelefono(telefono);
+
+            persona_agenda.put(nombre, telefono);
+            persona.setAgenda(persona_agenda);
+
+            salir = JOptionPane.showInputDialog("Ingrese 'Salir' para salir; Ingrese cualquier tecla para continuar asignando personas: ");
+            if (salir.equalsIgnoreCase("Salir")) {
+                break;
+            }
+        }
+
+        while (true) {
+            JOptionPane.showMessageDialog(null, "Ingrese 1 para Buscar contacto: ");
+            JOptionPane.showMessageDialog(null, "Ingrese 2 para Agregar contacto: ");
+            JOptionPane.showMessageDialog(null, "Ingrese 3 para Mostrar la agenda: ");
+
+            option = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la opcion: "));
+
+            switch (option) {
+                case 1:
+                    nombre = JOptionPane.showInputDialog("Ingrese un nombre a buscar:");
+                    int telefonoEncontrado = persona.BuscarNombre(nombre);
+                    JOptionPane.showMessageDialog(null, "Número de teléfono encontrado: " + telefonoEncontrado);
+                    break;
+                case 2:
+                    nombre = JOptionPane.showInputDialog("Ingrese un nombre:");
+                    telefono = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su nº de telefono: "));
+                    persona.AgregarContacto(nombre, telefono);
+                    break;
+                case 3:
+                    persona.MostrarAgenda();
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción incorrecta.");
+            }
+
+            salir = JOptionPane.showInputDialog("Ingrese 'Salir' para salir; Ingrese cualquier tecla para continuar: ");
+            if (salir.equalsIgnoreCase("Salir")) {
+                break;
+            }
+        }
+    }
+}
