386136de5c864dc41bd1bd60f5ee0e793327fb26
JoseFausti <141734639+JoseFausti@users.noreply.github.com>
2023-11-10 01:37:15 -0300

    Add files via upload

---
+++ Parcial 2 Java/src/Clase/Clase_Telefono_Movil.java
@@ -0,0 +1,63 @@
+package Clase;
+import javax.swing.*;
+import java.util.ArrayList;
+
+// Clase
+public class Clase_Telefono_Movil {
+    // Definimos las Variables
+    private String marca, modelo;
+    private int numero_serie;
+    // Creamos el ArrayList
+    private ArrayList<Clase_Telefono_Movil> telefono_movil;
+
+    // Constructor
+    public Clase_Telefono_Movil() {}
+    public Clase_Telefono_Movil(String marca, String modelo, int numero_serie) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.numero_serie = numero_serie;
+        this.telefono_movil = new ArrayList<>();
+    }
+
+    // Getter
+    public String getMarca() {return marca;}
+    public String getModelo() {return modelo;}
+    public int getNumero_serie() {return numero_serie;}
+    public ArrayList<Clase_Telefono_Movil> getTelefono_movil() {return telefono_movil;}
+
+    // Setter
+    public void setMarca(String marca) {this.marca = marca;}
+    public void setModelo(String modelo) {this.modelo = modelo;}
+    public void setNumero_serie(int numero_serie) {this.numero_serie = numero_serie;}
+    public void setTelefono_movil(ArrayList<Clase_Telefono_Movil> telefono_movil) {this.telefono_movil = telefono_movil;}
+
+    // Método
+    public void AgregarTelefono(String marca, String modelo, Integer numero_serie) {
+        if (!telefono_movil.contains(numero_serie)) {
+            telefono_movil.add(new Clase_Telefono_Movil(marca, modelo, numero_serie));
+            ;
+        }
+    }
+    public void EliminarTelefono(int numeroSerie) {
+        for (Clase_Telefono_Movil telefono : telefono_movil) {
+            if (telefono.getNumero_serie() == numeroSerie) {
+                telefono_movil.remove(telefono);
+                break;  // Se detiene después de eliminar el primer teléfono con el número de serie dado
+            }
+        }
+    }
+    public void BuscarTelefono(String marca, String modelo, Integer numero_serie) {
+        for (Clase_Telefono_Movil telefono : telefono_movil) {
+            if ((telefono_movil.equals(marca) && telefono_movil.equals(modelo)) || telefono_movil.equals(numero_serie)) {
+                JOptionPane.showMessageDialog(null, "Búsqueda exitosa \n" +
+                        "Telefono móvil: " + telefono);
+            }
+        }
+    }
+    public void MostrarTelefonos() {
+        JOptionPane.showMessageDialog(null,"Telefonos: ");
+        for (Clase_Telefono_Movil telefonos : telefono_movil){
+            JOptionPane.showMessageDialog(null,telefonos);
+        }
+    }
+}
---
+++ Parcial 2 Java/src/Telefono_Movil.java
@@ -0,0 +1,53 @@
+import Clase.Clase_Telefono_Movil;
+import javax.swing.*;
+
+// Programa Principal
+public class Telefono_Movil {
+    public static void main(String[] args) {
+        // Definimos las Variables
+        String marca,modelo,salir;
+        int numero_serie ,opcion;
+        // Creamos el Objeto
+        Clase_Telefono_Movil AgendaTelefonos = new Clase_Telefono_Movil();
+        // Menú de Opciones
+        while (true){
+        JOptionPane.showMessageDialog(null,"Ingrese 1 para" +
+                "agregar un nuevo elemento a la lista.");
+        JOptionPane.showMessageDialog(null,"Ingrese 2 para" +
+                "eliminar un elemento de la lista.");
+        JOptionPane.showMessageDialog(null,"Ingrese 3 para" +
+                "buscar un elemento por número o nombre.");
+        JOptionPane.showMessageDialog(null,"Ingrese 4 para" +
+                "listar todos los elemento en la lista.");
+        opcion = Integer.parseInt(JOptionPane.showInputDialog("Ingrese una opción: "));
+        switch (opcion){
+            case 1:
+                marca = JOptionPane.showInputDialog("Ingrese la marca: ");
+                modelo = JOptionPane.showInputDialog("Ingrese el modelo: ");
+                numero_serie = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de serie: "));
+                AgendaTelefonos.AgregarTelefono(marca,modelo,numero_serie);
+                break;
+            case 2:
+                numero_serie = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de serie del modelo" +
+                        "para eliminarlo: "));
+                AgendaTelefonos.EliminarTelefono(numero_serie);
+                break;
+            case 3:
+                marca = JOptionPane.showInputDialog("Ingrese la marca: ");
+                modelo = JOptionPane.showInputDialog("Ingrese el modelo: ");
+                numero_serie = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de serie: "));
+                AgendaTelefonos.BuscarTelefono(marca,modelo,numero_serie);
+                break;
+            case 4:
+                AgendaTelefonos.MostrarTelefonos();
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Opción incorrecta.");
+        }
+        // Mensaje de salida
+        salir = JOptionPane.showInputDialog("Ingrese 'Salir' para salir; Ingrese cualquier " +
+                "tecla para continuar asignando personas: ");
+        if (salir.equalsIgnoreCase("Salir")) {break;}
+        }
+    }
+}
